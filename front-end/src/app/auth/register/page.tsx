549752a69e55af0d5aa9cd4e7f14eab2a59b2534
GabJS10 <jgabis65@gmail.com>
2025-06-14 08:19:42 -0500

    Delete gitignore

---
+++ front-end/src/app/auth/register/page.tsx
@@ -0,0 +1,269 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+import { PhoneInput } from "@/components/ui/phone-input";
+import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { motion, AnimatePresence } from "motion/react";
+import axios from "axios";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const formSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, { message: "Name must be at least 2 characters long" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    phone: z.string().min(10, { message: "Phone number must be valid" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .regex(/[a-zA-Z0-9]/, { message: "Password must be alphanumeric" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
+
+export default function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const [progress, setProgress] = React.useState(0);
+  const [errorOrSuccess, setErrorOrSuccess] = React.useState<string | null>(
+    null
+  );
+  const [isAnimating, setIsAnimating] = React.useState(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!isAnimating) return;
+
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev < 90) return prev + 5;
+        clearInterval(interval); // Para en 90 hasta que el login termine
+        return prev;
+      });
+    }, 200); // velocidad de animación
+
+    return () => clearInterval(interval);
+  }, [isAnimating]);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      setIsAnimating(true); // inicia la animación
+      setErrorOrSuccess(null);
+      await axios.post("http://localhost:8000/autenticacion/api/register/", {
+        nombre: values.name,
+        correo: values.email,
+        password: values.password,
+      });
+
+      setIsAnimating(false); // detener animación
+      setProgress(100);
+
+      setTimeout(() => {
+        setProgress(0);
+        setErrorOrSuccess("success");
+        router.push("/auth/login");
+      }, 1000);
+    } catch (error) {
+      console.error("Form submission error", error);
+      setIsAnimating(false);
+      setProgress(0);
+      setErrorOrSuccess("error");
+    }
+  }
+
+  return (
+    <>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+            Create a new account by filling out the form below.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="grid gap-4">
+                {/* Name Field */}
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="name">Full Name</FormLabel>
+                      <FormControl>
+                        <Input id="name" placeholder="John Doe" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Email Field */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="email">Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          id="email"
+                          placeholder="johndoe@mail.com"
+                          type="email"
+                          autoComplete="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Phone Field */}
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="phone">Phone Number</FormLabel>
+                      <FormControl>
+                        <PhoneInput {...field} defaultCountry="TR" />
+                        {/* <Input
+                          id="phone"
+                          placeholder="555-123-4567"
+                          type="tel"
+                          autoComplete="tel"
+                          {...field}
+                        /> */}
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Password Field */}
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          id="password"
+                          placeholder="******"
+                          autoComplete="new-password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Confirm Password Field */}
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-2">
+                      <FormLabel htmlFor="confirmPassword">
+                        Confirm Password
+                      </FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          id="confirmPassword"
+                          placeholder="******"
+                          autoComplete="new-password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" className="w-full cursor-pointer">
+                  Register
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="login" className="underline">
+              Login
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+      {progress > 0 && (
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="mt-4"
+          >
+            <Progress value={progress} className="w-full" />
+          </motion.div>
+        </AnimatePresence>
+      )}
+      {errorOrSuccess && (
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className={`mt-4 p-4 rounded-md ${
+              errorOrSuccess === "error" ? "bg-red-500" : "bg-green-500"
+            }`}
+          >
+            <p>
+              {errorOrSuccess === "error"
+                ? "An error occurred while registering"
+                : "Registration successful! You can now log in."}
+            </p>
+          </motion.div>
+        </AnimatePresence>
+      )}
+    </>
+  );
+}
